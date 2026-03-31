f99c53e4ced884cad0a566eb0054fe7649f0dedb
Pinochino <Tranhunghp22112004@gmail.com>
2026-03-31 15:00:35 +0700

    feat(novel-detail): Implement Reading Progress service

--- SE2/src/main/java/com/example/SE2/repositories/ReadingProgressRepository.java
+++ SE2/src/main/java/com/example/SE2/repositories/ReadingProgressRepository.java
@@ -1,7 +1,13 @@
 package com.example.SE2.repositories;
 
+import com.example.SE2.models.Novel;
 import com.example.SE2.models.ReadingProgress;
+
+import com.example.SE2.models.User;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +17,10 @@ import java.util.Optional;
 public interface ReadingProgressRepository extends JpaRepository<ReadingProgress, Long> {
     Optional<ReadingProgress> findByUserIdAndChapterId(String userId, Long chapterId);
 
+
     List<ReadingProgress> findByUserIdAndChapterNovelId(String userId, Long novelId);
+    
+    Optional<ReadingProgress> findTopByUserAndChapter_NovelOrderByUpdatedAtDesc(
+            User user, Novel novel
+    );
 }
--- SE2/src/main/resources/liquibase/sql/init.sql
+++
@@ -1,3 +0,0 @@
-insert into Novel (title, status, description, author, cover_img_url)
-values ("Mắt biếc", "DROPPED", "Hi em", "Nguyễn Ngọc Ánh", "")
-
